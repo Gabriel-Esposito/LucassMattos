d1f6278e5bfe357c0372b990a7dc54a29acd2cbe
Gabriel <gabrieldeabreuesposito@gmail.com>
2023-12-14 15:56:30 -0300

    header

--- teste.js
+++ teste.js
@@ -1,4 +1,18 @@
 function animar(){
     const btn = document.getElementById("btn-menu")
     btn.classList.toggle('ativar')
+    menu()
 }
+
+let abrir = false
+function menu(){
+    let links = document.getElementById('links-menu')
+    if (abrir == false){
+        links.style.display = 'flex'
+        abrir = true
+    }else if (abrir == true){
+        links.style.display = 'none'
+        abrir = false
+    }
+}
+
